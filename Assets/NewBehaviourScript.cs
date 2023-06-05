3bc66fa4c09b0c7279fe50d2fe33aecf4b970280
hyunjun3108 <129152830+hyunjun3108@users.noreply.github.com>
2023-06-05 11:29:34 +0900

    Add files via upload
    random coin spawner

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject goldCoin;
+    public GameObject coinSet;
+
+    public float productionTime;
+    void Start()
+    {
+        StartCoroutine("CoinPro");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator CoinPro()
+    {
+        Coin();
+        yield return new WaitForSeconds(productionTime);
+        StartCoroutine("CoinPro");
+    }
+    void Coin()
+    {
+        GameObject coin = Instantiate(goldCoin) as GameObject;
+        coin.transform.parent = coinSet.transform;
+        coin.transform.position = new Vector3(transform.position.x, Random.Range(-3.9f, 3.9f), 0);
+        coin.GetComponent<CoinMove>().coinSpeed = Random.Range(40, 100);
+    }
+}
