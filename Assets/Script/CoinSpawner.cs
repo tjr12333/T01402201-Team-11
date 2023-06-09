46e0850af06d8f5f38a3300479d07489000dd527
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2023-06-10 03:03:41 +0900

    Spawn coin only game is active state

--- Assets/Script/CoinSpawner.cs
+++ Assets/Script/CoinSpawner.cs
@@ -5,36 +5,41 @@ using UnityEngine;
 public class CoinSpawner : MonoBehaviour
 {
     public GameObject coin;
-    [SerializeField]public float spawnRate = 3;
-    private float timer = 0;
+    [SerializeField]public float spawnRate = 3f;
+    private float timer = 0f;
+    public PlayerMovement player;
+
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
         spawnCoin();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer < spawnRate)
-        {
-            timer += Time.deltaTime;
-        } else {
-            spawnCoin();
-            timer = 0;
+        if (player.gameIsActive) {
+            if (timer < spawnRate)
+            {
+                timer += Time.deltaTime;
+            } else {
+                spawnCoin();
+                timer = 0f;
+            }
         }
     }
 
-    public float heightOffset = 5;
-    public float widthOffset = 8;
+    public float heightOffset = 5f;
+    public float widthOffset = 8.3f;
 
     void spawnCoin()
     {
         float leftPoint = transform.position.x - widthOffset;
         float rightPoint = transform.position.x + widthOffset;
-        // float lowestPoint = transform.position.y - heightOffset;
+        float lowestPoint = transform.position.y - heightOffset/2;
         float highestPoint = transform.position.y + heightOffset;
 
-        Instantiate(coin, new Vector3(Random.Range(leftPoint, rightPoint), Random.Range(transform.position.y, highestPoint), 0), transform.rotation);
+        Instantiate(coin, new Vector3(Random.Range(leftPoint, rightPoint), Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
