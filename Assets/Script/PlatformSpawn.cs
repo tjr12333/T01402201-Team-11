9f8bfb02ba8f5f5cde4cd8eba797593d2ba3682f
htoh702 <htoh702@gmail.com>
2023-06-09 21:44:52 +0900

    change platform spawn transform

--- Assets/Script/PlatformSpawn.cs
+++ Assets/Script/PlatformSpawn.cs
@@ -9,6 +9,10 @@ public class PlatformSpawn : MonoBehaviour
     public float spawnRate = 5; // 파이프 생성 속도
     private float timer = 0; // 타이머 변수
 
+    float[] values = {3, 9, -2, -9, 0};
+    float randomValue;
+    float previousValue;  //   이전에 선택된 값 저장 변수
+
     // Start 함수는 첫 번째 프레임 이전에 실행됩니다.
     void Start()
     {
@@ -40,11 +44,22 @@ public class PlatformSpawn : MonoBehaviour
     {
         float leftPoint = this.transform.position.x - offset; // 파이프의 왼쪽 끝점
         float rightPoint = this.transform.position.x + offset; // 파이프의 오른쪽 끝점
+        
+        while(true)
+        {
+            randomValue = values[Random.Range(0, values.Length)];
+            if(randomValue != previousValue){
+                break;
+            }
+        }
+
+        previousValue = randomValue;
 
         // 파이프를 생성하는 Instantiate 함수를 호출하여 파이프를 생성합니다.
 
         // 회전은 현재 오브젝트의 회전과 동일합니다.
-        Instantiate(platform, new Vector3(Random.Range(leftPoint, rightPoint),transform.position.y,0), transform.rotation);
+        //Instantiate(platform, new Vector3(Random.Range(leftPoint, rightPoint),transform.position.y,0), transform.rotation);
+        Instantiate(platform, new Vector3(transform.position.x + previousValue,transform.position.y,0), transform.rotation);
     }
 }
 
